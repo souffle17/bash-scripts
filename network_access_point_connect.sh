d75369409d90c3695e353dee6be581fac64557e8
souffle17 <144641936+souffle17@users.noreply.github.com>
2024-09-03 11:05:55 -0400

    handle networks not in range

--- network_access_point_connect.sh
+++ network_access_point_connect.sh
@@ -10,6 +10,13 @@ fi
 
 nmcli device wifi rescan
 
+SSID_EXISTS=$(nmcli -t -f SSID device wifi list | awk -v ssid="$SSID" -F ':' '$1 == ssid {print $1}')
+
+if [ -z "$SSID_EXISTS" ]; then
+    kdialog --error "SSID '$SSID' not found in the list of available networks."
+    exit 1
+fi
+
 # Check for the access point with the strongest signal
 STRONGEST_BSSID=$(nmcli -f SSID,BSSID,SIGNAL device wifi list | grep "$SSID" | sort -k3 -nr | head -n1 | awk '{print $2}')
 
